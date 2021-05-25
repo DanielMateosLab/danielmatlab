bf0c3ab36f1fbb0f5e04856d56fecdca985a7d4e
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-05-25 17:01:45 +0200

    Change how skills are rendered

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,24 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
+interface Skill {
+  title: string;
+  description: string;
+}
+const skills: Skill[] = [
+  {
+    title: "Confortable CLI usage",
+    description: `Navigate directories. Create, move, rename and delete files and
+      folders. Create links. Use grep. Use control and redirection
+      operators.`,
+  },
+  {
+    title: "Proficient Git",
+    description: `Handle version control with the CLI using atomic commits, branches,
+      pull requests, etc.`,
+  },
+];
+
 export default function Home() {
   return (
     <div>
@@ -10,20 +28,15 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1>Skills</h1>
-
-        <h2>Basic CLI usage</h2>
-        <p>
-          Navigate directories. Create, move, rename and delete files and
-          folders. Create links. Use grep. Use control and redirection
-          operators.
-        </p>
-
-        <h2>Proficent in Git and GitHub</h2>
-        <p>
-          Handle version control with the CLI using atomic commits, branches,
-          pull requests, etc.
-        </p>
+        <article>
+          <h1>Skills</h1>
+          {skills.map((skill) => (
+            <div key={skill.title}>
+              <h2> {skill.title} </h2>
+              <p> {skill.description} </p>
+            </div>
+          ))}
+        </article>
       </main>
     </div>
   );
