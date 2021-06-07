b70eeb1024b77eec32ac8cbdaa434d5ebf30a166
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-06-07 19:00:02 +0200

    Set up prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
-import { useEffect } from "react";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 interface Skill {
   title: string;
