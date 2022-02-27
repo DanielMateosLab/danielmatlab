54f067a236715959a132bded21c028c507e26ea2
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-02-27 19:07:41 +0100

    Create solution without remark

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
+import { GetStaticProps } from "next"
+import { useEffect, useState } from "react"
+
 export default function Home() {
+  const [content, setContent] = useState("")
+
+  useEffect(() => {
+    fetch(
+      "https://api.github.com/repos/DanielMateosLab/DanielMateosLab/readme",
+      {
+        headers: [["Accept", "application/vnd.github.v3.html"]],
+      }
+    )
+      .then((value) => value.text())
+      .then((value) => setContent(value))
+  }, [])
+
   return (
     <div>
-      <main>
-        <h1>Daniel Mateos Labrador</h1>
-      </main>
+      <main dangerouslySetInnerHTML={{ __html: content }} />
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  return {
+    props: {},
+  }
 }
