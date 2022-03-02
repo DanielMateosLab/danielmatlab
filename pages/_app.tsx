d7ab843ceb258ecc20684d96c7ba3901dfc6b5bd
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-03-02 21:45:04 +0100

    Move Head to _app and remove custom _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
+import Head from "next/head"
+import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Daniel Mateos Labrador</title>
+        <meta name="description" content="Portfolio Website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
