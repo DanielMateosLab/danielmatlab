311de1c6315c2b53ce0b81e2c412ae6269d26221
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-05-10 20:14:29 +0200

    Update print page

--- lib/print.ts
+++ lib/print.ts
@@ -3,7 +3,7 @@ import * as puppeteer from "puppeteer"
 async function printPdf() {
   const browser = await puppeteer.launch({ headless: true })
   const page = await browser.newPage()
-  await page.goto("http://localhost:3000/cv", { waitUntil: "networkidle0" })
+  await page.goto("http://localhost:3000", { waitUntil: "networkidle0" })
   const pdf = await page.pdf({
     format: "A4",
     printBackground: true,
