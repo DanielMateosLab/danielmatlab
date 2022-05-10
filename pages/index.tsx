9bdae4771bb027f19d8f9f88ffb7c4188349dc1c
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-05-10 20:00:19 +0200

    Add Github to contact section

--- pages/index.tsx
+++ pages/index.tsx
@@ -113,15 +113,19 @@ export default function Cv() {
                 Email:{" "}
                 <a href="mailto:daniel.mat.lab@gmail.com" target="_blank">
                   daniel.mat.lab@gmail.com
-                </a>
-                .<br />
-                Phone: +34 681 187 073.
+                </a>{" "}
+                <br />
+                Phone: +34 681 187 073
                 <br />
                 <a
                   href="https://www.linkedin.com/in/danielmateoslab/"
                   target="_blank"
                 >
                   LinkedIn
+                </a>{" "}
+                -{" "}
+                <a href="https://github.com/DanielMateosLab" target="_blank">
+                  GitHub
                 </a>
               </p>
             </article>
