fa5801d84c7c65fb1a33b64da5bd990f3a04641e
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-05-10 19:44:53 +0200

    Remove old index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,61 +1,180 @@
-import { GetStaticProps } from "next"
-import getHtmlFromReadme from "../lib/getHtmlFromReadme"
-import { spacing } from "../styles/constants"
+import CvHeader from "../lib/components/CvHeader"
+import ListItem from "../lib/components/ListItem"
+import { education, workExperience } from "../lib/pastExperiences"
+import { darkGray, gray } from "../styles/constants"
 
-interface Props {
-  homeContent: string
-}
-export default function Home({ homeContent }: Props) {
+export default function Cv() {
   return (
-    <div>
-      <main dangerouslySetInnerHTML={{ __html: homeContent }} />
+    <>
+      <div className="cv-root">
+        <CvHeader />
 
-      <style global jsx>
+        <main className="content">
+          <article>
+            <h2>A Brief Introduction</h2>
+            <div>
+              <ListItem>Learning web development since 2017.</ListItem>
+              <ListItem>Mostly self-taught and self-sufficient.</ListItem>
+              <ListItem>
+                Learn by doing. Check my{" "}
+                <a
+                  href="https://github.com/DanielMateosLab#-portfolio"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  portfolio
+                </a>
+                !
+              </ListItem>
+              <ListItem>
+                Finished a{" "}
+                <a
+                  href="https://4geeksacademy.com/us/coding-bootcamps/part-time-full-stack-developer"
+                  target="_blank"
+                >
+                  Full Stack Web Developer Bootcamp
+                </a>{" "}
+                in May 2022.
+              </ListItem>
+              <ListItem>
+                Former physical therapist, I am looking for a job in web
+                development because I am even more passionate about it.
+              </ListItem>
+            </div>
+          </article>
+
+          <article>
+            <h2>Technologies</h2>
+            <div>
+              <ListItem>
+                Languages: HTML5, CSS3, Javascript, Typescript, Python.
+              </ListItem>
+              <ListItem>API "Patterns": REST, GraphQL.</ListItem>
+              <ListItem>
+                Frameworks: React, Express, Next.js, Apollo, Jest, Flask,
+                SQLAlchemy, Pytest, Emotion, Bootstrap, Material-UI.
+              </ListItem>
+              <ListItem>Databases: MongoDB, PostgreSQL.</ListItem>
+              <ListItem>Other: Linux, Docker, Node, Git</ListItem>
+            </div>
+          </article>
+
+          <article>
+            <h2>Languages</h2>
+            <div>
+              Spanish (native), English (professional), French (elementary) and
+              Italian (elementary).
+            </div>
+          </article>
+
+          <article>
+            <h2>Availability</h2>
+            <p>
+              Immediately available. Open to work remotely anywhere or to
+              relocate within Spain or Italy.
+            </p>
+          </article>
+
+          <article>
+            <h2>Experience</h2>
+            <div className="spaced-list">
+              {workExperience.map((item) => (
+                <div key={item.company} className="list-item">
+                  <span className="secondary-text">
+                    {item.startDate} - {item.endDate}
+                  </span>
+                  <span>{item.position}</span>
+                  <span className="degree">{item.company}</span>
+                  <span className="secondary-text">{item.location}</span>
+                </div>
+              ))}
+            </div>
+          </article>
+
+          <div>
+            <article>
+              <h2>Education</h2>
+              <div className="spaced-list">
+                {education.map((item) => (
+                  <div key={item.degree} className="list-item">
+                    <span className="secondary-text">
+                      {item.startDate} - {item.endDate}
+                    </span>
+                    <span>{item.degree}</span>
+                    <span className="degree">{item.school}</span>
+                  </div>
+                ))}
+              </div>
+            </article>
+
+            <article>
+              <h2>Contact</h2>
+              <p>
+                Email:{" "}
+                <a href="mailto:daniel.mat.lab@gmail.com" target="_blank">
+                  daniel.mat.lab@gmail.com
+                </a>
+                .<br />
+                Phone: +34 681 187 073.
+                <br />
+                <a
+                  href="https://www.linkedin.com/in/danielmateoslab/"
+                  target="_blank"
+                >
+                  LinkedIn
+                </a>
+              </p>
+            </article>
+          </div>
+        </main>
+      </div>
+
+      <style jsx>
         {`
-          body {
-            background: rgb(232, 237, 255);
+          .content {
+            max-width: 80%;
+            margin: auto;
+            margin-bottom: 3rem;
           }
-          main {
-            padding: 1rem;
-          }
-          article {
-            box-shadow: 4px 4px 6px 2px rgb(0 0 0 / 20%);
-            padding: ${spacing};
-            background: white;
-            margin: 1rem 0;
-            border-radius: 4px;
-          }
-
-          h1 {
-            margin: 2rem 0;
-          }
-          h2,
-          h4 {
-            margin: 0;
-          }
-          p {
-            margin-top: ${spacing};
-            margin-bottom: 0;
-          }
-
-          @media screen and (min-width: 796px) {
-            main {
-              max-width: 796px;
-              margin: auto;
+          @media (min-width: 768px) {
+            .content {
+              display: grid;
+              grid-template-columns: 1fr 1fr;
+              column-gap: 3rem;
             }
+          }
+
+          .list-item {
+            display: flex;
+            flex-direction: column;
+            gap: 0.125rem;
+          }
+          .spaced-list > :not(:last-child) {
+            margin-bottom: 1rem;
+          }
+
+          .secondary-text {
+            font-size: 0.8rem;
+            color: ${darkGray};
+          }
+          .degree {
+            font-weight: 300;
           }
         `}
       </style>
-    </div>
+
+      <style global jsx>
+        {`
+          .cv-root {
+            background: white;
+          }
+          .cv-root h2 {
+            color: ${gray};
+            text-transform: uppercase;
+            font-size: 1.2rem;
+          }
+        `}
+      </style>
+    </>
   )
-}
-
-export const getStaticProps: GetStaticProps<Props> = async (context) => {
-  const homeContent = await getHtmlFromReadme("DanielMateosLab")
-
-  return {
-    props: {
-      homeContent,
-    },
-  }
 }
