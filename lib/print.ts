581d6f25b03284cf6b60e7421c5b893deea33c74
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-11-03 11:01:15 +0100

    Minor refactors

--- lib/print.ts
+++ lib/print.ts
@@ -1,34 +1,41 @@
 import * as puppeteer from "puppeteer"
 
+const pdfSettings = {
+  format: "A4",
+  printBackground: true,
+  omitBackground: false,
+  scale: 0.77,
+} as const
+enum PrintVersion {
+  WithPicture = "WithPicture",
+  WithoutPicture = "WithoutPicture",
+}
+const outputPath = {
+  [PrintVersion.WithPicture]: "./daniel_mateos_labrador_wp.pdf",
+  [PrintVersion.WithoutPicture]: "./daniel_mateos_labrador.pdf",
+}
+
 async function printPdf() {
   const browser = await puppeteer.launch({ headless: true })
   const page = await browser.newPage()
 
-  // Print without profile picture
-  await page.goto("http://localhost:3000", { waitUntil: "networkidle0" })
-  await page.pdf({
-    format: "A4",
-    printBackground: true,
-    omitBackground: false,
-    scale: 0.77,
-    path: "./daniel_mateos_labrador.pdf",
-  })
-
-  // Print with profile picture
-  await page.goto("http://localhost:3000?profilePicture=True", {
-    waitUntil: "networkidle0",
-  })
-  await page.pdf({
-    format: "A4",
-    printBackground: true,
-    omitBackground: false,
-    scale: 0.77,
-    path: "./daniel_mateos_labrador_wp.pdf",
-  })
+  await printVersion(PrintVersion.WithPicture, page)
+  await printVersion(PrintVersion.WithoutPicture, page)
 
   await browser.close()
 }
 
-;(async () => {
-  const pdf = await printPdf()
-})()
+async function printVersion(version: PrintVersion, page: puppeteer.Page) {
+  const queryParams =
+    version === PrintVersion.WithPicture ? "?profilePicture=True" : ""
+  const url = `http://localhost:3000${queryParams}`
+  await page.goto(url, { waitUntil: "networkidle0" })
+  await page.pdf({
+    ...pdfSettings,
+    path: outputPath[version],
+  })
+}
+
+printPdf()
+  .then(() => console.log("Done"))
+  .catch((error) => console.error(error))
