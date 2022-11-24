bf4da47bfd1ac44949ebfd532371c4dbb3c1470d
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-11-24 19:39:16 +0100

    Update availability

--- lib/components/Availability.tsx
+++ lib/components/Availability.tsx
@@ -2,12 +2,7 @@ export default function Availability() {
   return (
     <article>
       <h2>Availability</h2>
-      <p>
-        Immediately available to start part-time with the possibility of going
-        full-time within a month.
-        <br />
-        Open to work remotely or to relocate.
-      </p>
+      <p>Immediately available. Open to work remotely or to relocate.</p>
     </article>
   )
 }
