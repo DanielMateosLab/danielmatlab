8b94edbaae5ee362b25a1a58d209460c416e4bef
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-11-24 19:47:06 +0100

    Re-order sections. Replace Introduction for Achievements

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import { useRouter } from "next/router"
 import React, { useEffect, useState } from "react"
+import Achievements from "../lib/components/Achievements"
 import Availability from "../lib/components/Availability"
 import Contact from "../lib/components/Contact"
 import CvHeader from "../lib/components/CvHeader"
 import Education from "../lib/components/Education"
 import Experience from "../lib/components/Experience"
-import Introduction from "../lib/components/Introduction"
 import Languages from "../lib/components/Languages"
 import Technologies from "../lib/components/Technologies"
 import { darkGray, gray } from "../styles/constants"
@@ -24,27 +24,27 @@ export default function Cv() {
   const Items = {
     [ScreenSizes.Small]: (
       <>
-        <Introduction />
+        <Experience />
+        <Achievements />
         <Technologies />
+        <Education />
         <Languages />
         <Availability />
-        <Education />
-        <Experience />
         <Contact />
       </>
     ),
     [ScreenSizes.Big]: (
       <>
         <div className="column">
-          <Introduction />
-          <Languages />
+          <Experience />
           <Education />
+          <Languages />
           <Contact />
         </div>
         <div className="column">
+          <Achievements />
           <Technologies />
           <Availability />
-          <Experience />
         </div>
       </>
     ),
