80ce95a053122e7f5d214809c880d3a00f2958cc
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-11-24 19:35:29 +0100

    Update experience

--- lib/components/Experience.tsx
+++ lib/components/Experience.tsx
@@ -5,9 +5,9 @@ export default function Experience() {
 
       <div className="spaced-list">
         <div className="list-item">
-          <span className="secondary-text">Jun 2022 - Present</span>
+          <span className="secondary-text">Jun 2022 - November 2022</span>
           <span>
-            Working as Fronted Developer for{" "}
+            Worked as Fronted Developer for{" "}
             <a href="https://attlos.com/" target="_blank">
               Attlos
             </a>
@@ -21,18 +21,22 @@ export default function Experience() {
                 Write code using TDD (with jest) and CDD (with Storybook)
                 approaches.
               </li>
-              <li>Review Pull Requests.</li>
+              <li>
+                Review Pull Requests to ensure code quality and a pixel perfect
+                match with Figma's design.
+              </li>
               <li>Version control using Git.</li>
             </ul>
           </div>
         </div>
 
         <div className="list-item">
-          <span className="secondary-text">May 2022</span>
+          <span className="secondary-text">Oct 2021 - May 2022</span>
           <span>
-            Worked as a freelance developing{" "}
+            Worked as a freelance Full Stack Developer developing websites such
+            as{" "}
             <a href="https://fada-var.fr/" target="_blank">
-              FADA's website
+              https://fada-var.fr
             </a>
             .
           </span>
