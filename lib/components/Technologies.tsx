009ef89dfe48c15f8f80e294678819dc1f65d66f
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-05-23 23:11:12 +0200

    Update technologies

--- lib/components/Technologies.tsx
+++ lib/components/Technologies.tsx
@@ -6,14 +6,15 @@ export default function Technologies() {
       <h2>Technologies</h2>
       <div>
         <ListItem>
-          Languages: Typescript, Python, Javascript, HTML5, CSS3, SCSS.
+          Languages: Typescript, Python, Javascript, C, HTML5, CSS3, SCSS.
         </ListItem>
         <ListItem>
-          Frameworks: React, Next.js, Svelte, Express, Apollo, Jest, Flask,
-          SQLAlchemy, Pytest, TailwindCSS, Material-UI, Primereact, CSS in JS.
+          Frameworks: React, Next.js, Express, Apollo, Jest, Prisma,
+          TailwindCSS, Shadcn, Radix, Material-UI, Primereact, CSS in JS, Flask,
+          SQLAlchemy, Pytest.
         </ListItem>
         <ListItem>Databases: MongoDB, PostgreSQL.</ListItem>
-        <ListItem>API "Patterns": REST, GraphQL.</ListItem>
+        <ListItem>API "Patterns": REST, GraphQL, RPC.</ListItem>
         <ListItem>Other: AWS, OpenAI APIs, Linux, Docker, Node, Git.</ListItem>
       </div>
     </article>
